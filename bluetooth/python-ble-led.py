787f70b5a2d8415525d36efa2ff100288fce2b44
Komsan Kanjanasit <94804188+komsan-k@users.noreply.github.com>
2025-11-13 18:57:43 +0700

    Update python-ble-led.py

--- bluetooth/python-ble-led.py
+++ bluetooth/python-ble-led.py
@@ -1,3 +1,11 @@
+
+import asyncio
+from bleak import BleakClient
+
+SERVICE_UUID = "12345678-1234-5678-1234-56789abcdef0"
+CHARACTERISTIC_UUID = "12345678-1234-5678-1234-56789abcdef1"
+DEVICE_ADDRESS = "80:7d:3a:f4:e8:2e"  # Replace with ESP32 MAC
+
 async def run():
     async with BleakClient(DEVICE_ADDRESS) as client:
         print(f"Connected: {client.is_connected}")
@@ -14,4 +22,3 @@ async def run():
             await asyncio.sleep(2)
 
 asyncio.run(run())
-
